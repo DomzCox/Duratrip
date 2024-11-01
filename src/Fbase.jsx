552623e2e2e1395e8ae198d92c9c8a6aac7d343b
Nyron Telemaque <nyron.biz@gmail.com>
2024-11-01 13:14:03 -0500

    Firebase database set up and is working properly. Also added a cities collection and state to the hard coded file.

--- src/Fbase.jsx
+++ src/Fbase.jsx
@@ -1,30 +1,23 @@
-import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { firebaseConfig } from './assets/firebaseConfig';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB6n_M00lv9-To_p2_XYGguwh3JodAeres",
-  authDomain: "duratrip-fe89c.firebaseapp.com",
-  projectId: "duratrip-fe89c",
-  storageBucket: "duratrip-fe89c.firebasestorage.app",
-  messagingSenderId: "378317648749",
-  appId: "1:378317648749:web:da26383131c56e7d8fe064",
-  measurementId: "G-JM0TCZD6QC"
-};
+// TODO: Replace the following with your app's Firebase project configuration
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-//const analytics = getAnalytics(app);
+
+const collect = collection(db, 'Equipments/Sterilizers/Tuttnauer');
+const shot = await getDocs(collect)
+const response = await shot.docs.map( doc => doc.data())
+console.log(response)
 
 function Fbase() {
   return (
-    <div>Fbase</div>
+    <div className='container'>Firebasedb</div>
   )
 }
 
-export default Fbase
+export default Fbase;
---
+++ src/assets/firebaseConfig.jsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB6n_M00lv9-To_p2_XYGguwh3JodAeres",
+    authDomain: "duratrip-fe89c.firebaseapp.com",
+    projectId: "duratrip-fe89c",
+    storageBucket: "duratrip-fe89c.firebasestorage.app",
+    messagingSenderId: "378317648749",
+    appId: "1:378317648749:web:da26383131c56e7d8fe064",
+    measurementId: "G-JM0TCZD6QC",
+  };
