301e0ce662e111c68d17d9df7caa48baf3a0ac2b
Nyron Telemaque <nyron.biz@gmail.com>
2024-11-01 13:27:35 -0500

    Set the fetching data phase of the app inside of the useEffect hook. This way it renders after to component mounts

--- src/Fbase.jsx
+++ src/Fbase.jsx
@@ -1,6 +1,7 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { firebaseConfig } from './assets/firebaseConfig';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { firebaseConfig } from "./assets/firebaseConfig";
+import { useEffect } from "react";
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 
@@ -9,15 +10,24 @@ import { firebaseConfig } from './assets/firebaseConfig';
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const collect = collection(db, 'Equipments/Sterilizers/Tuttnauer');
-const shot = await getDocs(collect)
-const response = await shot.docs.map( doc => doc.data())
-console.log(response)
+const equipment_collection = collection(db, "Equipments/Sterilizers/Tuttnauer");
+const user_collection = collection(db, "Users");
+
+async function getCollections(collectiondata) {
+  const shot = await getDocs(collectiondata);
+  const response = await shot.docs.map((doc) => doc.data());
+  console.log(response);
+}
 
 function Fbase() {
-  return (
-    <div className='container'>Firebasedb</div>
-  )
+
+  useEffect(() => {
+    getCollections(equipment_collection);
+    getCollections(user_collection);
+  }, [equipment_collection, equipment_collection]);
+
+  
+  return <div className="container">Firebasedb</div>;
 }
 
 export default Fbase;
